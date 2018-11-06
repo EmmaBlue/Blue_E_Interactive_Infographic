1fffc0f14b791ba7f6153fd4a9ede08d7415309d
Emma Blue <emmajaeblue@gmail.com>
2018-11-05 20:06:57 -0500

    animating each section

--- js/animate-on-load.js
+++ js/animate-on-load.js
@@ -1,6 +1,38 @@
 
   (() => {
 
+    function setVisible (element) {
+
+        console.log("setVisible");
+
+        element.classList.remove("none");
+        element.classList.add("animated");
+        element.classList.add("fadeIn");
+    }
+
+
+
   
-  
+    var waypoint = new Waypoint({
+      element: document.querySelector("#opsSect"),
+      handler: function(direction) {
+          console.log("scrolled to element!", this.element);
+
+              setVisible(this.element);
+        
+      },
+
+
+  });
+
+  var waypoint2 = new Waypoint({
+    element: document.querySelector("#topsFullSect"),
+    handler: function(direction) {
+        console.log("scrolled to element!", this.element);
+
+            setVisible(this.element);
+      
+    },
+  });
+
   })();
